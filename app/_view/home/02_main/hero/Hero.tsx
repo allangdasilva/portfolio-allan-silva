ad5b1245b21852d70b4749202b6d722030ff02db
Allan Silva <allandasilva33@outlook.com>
2026-08-18 22:55:17 -0300

    fix: corrige estilos no Hero

--- app/_view/home/02_main/hero/Hero.tsx
+++ app/_view/home/02_main/hero/Hero.tsx
@@ -1,6 +1,6 @@
 export default function Hero() {
   return (
-    <div className="bg-tertiary text-primary p-base flex min-h-dvh">
+    <div className="bg-tertiary text-primary p-base sticky top-0 flex min-h-dvh">
       <div className="max-w-384 relative mx-auto flex w-full flex-1 flex-col justify-between">
         <header className="z-1 relative">
           <h1 className="type-xl flex flex-col gap-1 lg:gap-2">
