9fbf2ff885991bbd6e288174072bf326b9aeb9b8
Allan Silva <allandasilva33@outlook.com>
2026-08-18 22:54:48 -0300

    feat: adiciona componente AboutMe

--- app/_view/home/02_main/Main.tsx
+++ app/_view/home/02_main/Main.tsx
@@ -1,9 +1,11 @@
+import AboutMe from './about-me/AboutMe';
 import Hero from './hero/Hero';
 
 export default function Main() {
   return (
     <main>
       <Hero />
+      <AboutMe />
     </main>
   );
 }
---
+++ app/_view/home/02_main/about-me/AboutMe.tsx
@@ -0,0 +1,25 @@
+import { aboutMeCopy } from '@/app/data/about-me';
+
+export default function AboutMe() {
+  const copy = aboutMeCopy;
+
+  return (
+    <div className="bg-primary text-secondary px-base lg:py-31 relative py-24">
+      <div className="max-w-384 gap-x-base mx-auto grid grid-cols-4 gap-y-3 lg:grid-cols-12">
+        <div className="col-span-full col-start-3 lg:col-start-8 lg:justify-self-end">
+          <h2 className="type-base font-serif italic">
+            <span aria-hidden>“</span> Sobre mim
+          </h2>
+        </div>
+
+        <div className="col-span-full flex flex-col gap-3 lg:col-start-1 lg:col-end-8 lg:row-start-1 lg:gap-4">
+          {copy.map((current, index) => (
+            <p key={index} className="type-lg max-w-[64ch]">
+              {current}
+            </p>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
