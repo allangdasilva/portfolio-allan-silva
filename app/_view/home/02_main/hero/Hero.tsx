e97067ab120c3c7b571f2676e6c6fb5454808d10
Allan Silva <allandasilva33@outlook.com>
2026-08-21 00:09:09 -0300

    feat: adiciona efeito de fade up no Hero

--- app/_view/home/02_main/hero/Hero.tsx
+++ app/_view/home/02_main/hero/Hero.tsx
@@ -1,14 +1,49 @@
+'use client';
+
+import { motion, useMotionValueEvent, useScroll, Variants } from 'motion/react';
+import { useState } from 'react';
+
 export default function Hero() {
+  const [hasScrolled, setHasScrolled] = useState(false);
+  const { scrollY } = useScroll();
+
+  const FADE_UP: Variants = {
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.5, ease: 'easeInOut' },
+    },
+    hidden: (direction: number) => ({
+      opacity: 0,
+      y: direction,
+      transition: { duration: 0.5, ease: 'easeInOut' },
+    }),
+  };
+
+  useMotionValueEvent(scrollY, 'change', (latest) => {
+    if (latest > 1 && !hasScrolled) {
+      setHasScrolled(true);
+    } else if (latest <= 1 && hasScrolled) {
+      setHasScrolled(false);
+    }
+  });
+
   return (
     <section className="bg-tertiary text-primary p-base sticky top-0 flex min-h-dvh">
       {/* wrapper max-w */}
       <div className="max-w-component relative flex w-full flex-1 flex-col justify-between">
         {/* header */}
         <header className="z-1 relative">
-          <h1 className="type-xl flex flex-col gap-y-1 lg:gap-y-2">
+          <motion.h1
+            variants={FADE_UP}
+            initial={'show'}
+            animate={hasScrolled ? 'hidden' : 'show'}
+            custom={-18}
+            className="type-xl flex flex-col gap-y-1 lg:gap-y-2"
+          >
             <span>Allan Silva</span>
             <span>Front end & UI/UX</span>
-          </h1>
+          </motion.h1>
         </header>
 
         {/* banner */}
@@ -18,9 +53,15 @@ export default function Hero() {
 
         {/* high-light */}
         <div className="z-1 relative">
-          <p className="type-3xl uppercase [text-align-last:justify]">
+          <motion.p
+            variants={FADE_UP}
+            initial={'show'}
+            animate={hasScrolled ? 'hidden' : 'show'}
+            custom={18}
+            className="type-3xl uppercase [text-align-last:justify]"
+          >
             THE ART OF CODING
-          </p>
+          </motion.p>
         </div>
       </div>
     </section>
