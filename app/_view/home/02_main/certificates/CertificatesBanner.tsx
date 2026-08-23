2af5bf573ef9e6845edc8ba2ba0d98b46465701d
Allan Silva <allandasilva33@outlook.com>
2026-08-23 20:03:58 -0300

    fix: altera banner de CertificatesBanner

--- app/_view/home/02_main/certificates/CertificatesBanner.tsx
+++ app/_view/home/02_main/certificates/CertificatesBanner.tsx
@@ -1,4 +1,5 @@
-import fluidBg from '@/app/images/fluid-purple-green.avif';
+import highlightBg from '@/app/images/highlight-bg.webp';
+import highlight from '@/app/images/highlight.webp';
 
 export default function CertificatesBanner({
   ...props
@@ -7,13 +8,13 @@ export default function CertificatesBanner({
     <div {...props}>
       {/* first image */}
       <div
-        style={{ backgroundImage: `url(${fluidBg.src})` }}
+        style={{ backgroundImage: `url(${highlight.src})` }}
         className="z-2 w-8/10 relative aspect-video bg-contain"
       ></div>
       {/* second image */}
       <div
-        style={{ backgroundImage: `url(${fluidBg.src})` }}
-        className="z-1 max-h-4/10 absolute inset-0 top-1/2 -translate-y-1/2 bg-cover"
+        style={{ backgroundImage: `url(${highlightBg.src})` }}
+        className="z-1 max-h-4/10 bg-tertiary absolute inset-0 top-1/2 -translate-y-1/2 bg-cover bg-blend-hard-light"
       ></div>
     </div>
   );
