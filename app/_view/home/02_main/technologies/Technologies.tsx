c330ccd0753149f5eebbda68f4ac12eb3c985680
Allan Silva <allandasilva33@outlook.com>
2026-08-24 02:39:38 -0300

    fix: corrige estilo do separador em Technologies

--- app/_view/home/02_main/technologies/Technologies.tsx
+++ app/_view/home/02_main/technologies/Technologies.tsx
@@ -10,7 +10,7 @@ export default function Technologies() {
       {/* wrapper grid/px-base */}
       <div className="gap-x-base px-base grid grid-cols-4 gap-y-3 overflow-hidden lg:grid-cols-12 lg:gap-y-0">
         {/* title max-w/lg:row-start-1 */}
-        <div className="max-w-component col-span-full col-start-3 w-full lg:col-start-1 lg:row-start-1">
+        <div className="max-w-component col-span-full col-start-3 w-full lg:z-10 lg:col-start-1 lg:col-end-6 lg:row-start-1">
           <TextFade direction="up">
             <TitleSection className="type-base text-secondary">
               Tecnologias
@@ -26,7 +26,7 @@ export default function Technologies() {
                 key={index}
                 className="relative col-span-full py-4 first:pt-0 lg:col-start-7 lg:py-6 lg:even:col-start-6"
               >
-                <span className="border-secondary absolute left-0 top-0 h-full w-[calc(100%+32px)] border-b-2"></span>
+                <span className="border-secondary pointer-events-none absolute left-0 top-0 h-full w-[calc(100%+32px)] border-b-2"></span>
                 <TextFade direction="up">
                   <span className="type-2xl text-secondary">{current}</span>
                 </TextFade>
